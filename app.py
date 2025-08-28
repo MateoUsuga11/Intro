5c6b179f940b203bdc71f49c8e72c5c46f36b177
MateoUsuga11 <musugav1@eafit.edu.co>
2025-08-28 12:51:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Que mas mis papus")
+st.title("Dark Souls")
 
 st.header("Cómo están")
 st.write("papus")
 image = Image.open('%3Fscar.webp')
 
-st.image(image, caption='Foxy jumpscare')
+st.image(image, caption='Oscar De Astora')
