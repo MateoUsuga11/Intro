cc592e9f68038052ce57fc4e17815f604633dcee
MateoUsuga11 <musugav1@eafit.edu.co>
2025-08-28 12:47:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title("Que mas mis papus")
 
 st.header("Cómo están")
 st.write("papus")
-image = Image.open('fnaf-memes.gif')
+image = Image.open('%3Fscar.webp')
 
 st.image(image, caption='Foxy jumpscare')
