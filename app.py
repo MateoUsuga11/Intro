4a87f9c885eb6838af538ef3544c6c9810fda645
MateoUsuga11 <musugav1@eafit.edu.co>
2025-08-28 12:53:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 st.title("Dark Souls")
 
-st.header("Cómo están")
-st.write("papus")
+st.header("Oscar De Astora")
+st.write("un caballero de alto rango de Astora, dispuesto de buscar en Lordran una vez que se volvió un no muerto")
 image = Image.open('%3Fscar.webp')
 
 st.image(image, caption='Oscar De Astora')
